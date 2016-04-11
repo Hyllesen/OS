c6737c44faf3240f5dfd36cbb7a700bff5794ff7
user <user@debian.compute.dtu.dk>
2016-04-11 08:52:07 +0200

    Video opdateret

--- src/kernel/video.c
+++ src/kernel/video.c
@@ -14,6 +14,9 @@
 /*! Max number of columns in the VGA buffer. */
 #define MAX_ROWS                (25)
 
+int strlen(const char *str);
+void WriteCharacter(unsigned char c, unsigned char forecolour, unsigned char backcolour, int x, int y);
+
 struct screen_position
 {
  unsigned char character; /*!< The character part of the byte tuple used for
@@ -34,14 +37,79 @@ struct screen
 static struct screen* const
 screen_pointer = (struct screen*) 0xB8000;
 
-void
-kprints(const char* string)
-{
- /* Implement me! */
+int xPosition, yPosition = 0;
+int textColor = 2;
+int backgroundColor = 0;
+
+void cls();
+
+/* Clear the screen */
+void cls() {
+	int i;
+	int h;	
+	
+	for (h = 0; h < MAX_COLS; h++) {
+		for(i = 0; i < MAX_ROWS; i++) {
+			WriteCharacter(' ', textColor, backgroundColor,h,i);
+		}
+	}
 }
 
 void
-kprinthex(const register uint32_t value)
+kprints(const char* string) {
+	int x;
+	int stringLength = strlen(string);
+	for (x = 0; x < stringLength; x++) {
+		if(string[x] == '\n') {
+			yPosition++;
+			xPosition = 0;
+			break;	//Don't print '\n', it will become an o/0 on the screen) 
+		}
+		WriteCharacter(string[x], textColor, backgroundColor,xPosition,yPosition);
+		xPosition++;
+	}
+} 
+
+
+/* Returns length of string until it find a 0. */
+int strlen(const char *str) {
+		int numberOfChars = 0;
+		while(str[numberOfChars] != '\0') {
+			++numberOfChars;
+		}
+		return numberOfChars;
+}
+
+void WriteCharacter(unsigned char c, unsigned char forecolour, unsigned char backcolour, int x, int y)
 {
- /* Implement me! */
+     uint16_t attrib = (backcolour << 4) | (forecolour & 0x0F);
+     volatile uint16_t * where;
+     where = (volatile uint16_t *)0xB8000 + (y * 80 + x) ;
+     *where = c | (attrib << 8);
+}
+
+void
+kprinthex(const register uint32_t *value)
+{
+	
+	
+	/*
+	unsigned char buf[4];
+ 
+	 pack into buf string 
+	buf[0] = value >> 24;
+	buf[1] = value >> 16;
+	buf[2] = value >> 8;
+	buf[3] = value;
+	 */
+	
+	int x;	
+	for(x = 0; x < 4; x++) {
+		WriteCharacter('x', textColor, backgroundColor,xPosition,yPosition);		
+		xPosition++;
+	}
+	yPosition++;
+	xPosition = 0;
+	
+	
 }
